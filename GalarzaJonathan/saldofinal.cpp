7556885a31db0b41834380620c4a017bcb717317
Jonathangr18 <timoteogr699@gmail.com>
2022-10-25 22:42:46 -0500

    Act.B2

---
+++ GalarzaJonathan/saldofinal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+        float x,s=0;
+        int i=0,l;
+        cout<<"Ingrese cantidad de egresos (l): ";
+        cin>>l;
+        cout<<"Ingrese saldo inical (s): ";
+        cin>>s;
+        do{
+                cout<<"Ingrese egresos (x): ";
+                cin>>x;
+                i=i+1;
+                s=s+x;
+
+        }while(i<l);
+        cout<<"El saldo final es: "<<s<<endl;
+
+        return 0;
+}
